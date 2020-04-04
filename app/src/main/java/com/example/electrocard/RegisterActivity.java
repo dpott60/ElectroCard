f2350a8ec08bbf978a7f6f22ac1ecd40ef41a278
Dustin Potter <42753502+dpott60@users.noreply.github.com>
2020-04-03 19:03:29 -0500

    Updated RegisterActivity
    Added try catch block to validate that the new username and new password areas are not null and are valid.

--- app/src/main/java/com/example/electrocard/RegisterActivity.java
+++ app/src/main/java/com/example/electrocard/RegisterActivity.java
@@ -5,8 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -23,8 +25,21 @@ public class RegisterActivity extends AppCompatActivity {
         makeAccountBTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent ini = new Intent(myContext, MainActivity.class);
-                startActivity(ini);
+                EditText newUsernameET = findViewById(R.id.newUsernameET);
+                EditText newPasswordET = findViewById(R.id.newPasswordET);
+                String username;
+                String password;
+                try{
+                    username = newUsernameET.getText().toString();
+                    password = newPasswordET.getText().toString();
+                    if (username != null && password != null) {
+                        Intent ini = new Intent(myContext, MainActivity.class);
+                        startActivity(ini);
+                    }
+
+                } catch (Error e) {
+                    Log.d("Error", "Invalid username/password");
+                }
             }
         });
     }
